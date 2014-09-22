bf1b6fff1b29eaed4c30d8b2e5e7b6d4c85da834
Jan Marten <jan.marten@gmx.de>
2014-09-22 18:09:31 +0200

    pull files

--- install.bat
+++ install.bat
@@ -4,3 +4,13 @@ cinst SublimeText3
 cinst clink
 cinst sudo
 cinst git
+
+set PATH=%PATH%;%programfiles(x86)%\Git\cmd\
+
+cd /D %USERPROFILE%\Documents
+if not exist GitHub mkdir GitHub
+cd GitHub
+
+git clone https://github.com/LeovR/dotfiles.git
+cd dotfiles
+call sync -f
