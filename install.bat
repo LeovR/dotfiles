03a7927d96f96f69981a81aebc1d74b5282cc001
Jan Marten <jan.marten@gmx.de>
2014-10-16 20:15:44 +0200

    Install sudo

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 cinst git
+cinst sudo
 
 IF "%PROCESSOR_ARCHITECTURE%"=="x86" (set PATH=%PATH%;%programfiles%\Git\cmd\) else (set PATH=%PATH%;%programfiles(x86)%\Git\cmd\)
 
